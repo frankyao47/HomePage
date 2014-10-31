0abd82c23290779f44d3d80a59f283ec9fdf7a5b
Frank Yao <yaofangjie2010@126.com>
2014-10-31 23:07:08 +0800

    a bee is following your mouse

---
+++ move.js
@@ -0,0 +1,13 @@
+var imageName = "newbee";
+
+function mouseIn(event){ 
+	var image = document.getElementById(imageName);
+
+	var mouseX = event.clientX; 
+  	var mouseY = event.clientY;
+	
+	image.style.left = mouseX + "px";
+	
+	image.style.top = mouseY + "px";
+	//document.write("Hello world!");
+}
